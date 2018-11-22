bb8b0cbbd4abd54dfb0862192ccdfc5ead2bee7f
Hyeji Do <hjdo.dev@gmail.com>
2018-11-22 17:08:44 +0900

    Test

--- app/src/main/java/com/example/hjdo/doslist/login/LoginMainActivity.kt
+++ app/src/main/java/com/example/hjdo/doslist/login/LoginMainActivity.kt
@@ -26,6 +26,7 @@ class LoginMainActivity: AppCompatActivity() {
             checkUser()
         }
 
+
         emailEditText.setText("hjdo@konai.com")
         passwdEditText.setText("1234")
     }
